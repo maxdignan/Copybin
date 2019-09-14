8c213fa6333e7d2313351d0e398fe49f2faa78da
Max Dignan <maxdignan@gmail.com>
2019-09-14 18:21:55 -0400

    Initial Commit

---
+++ copyclip_opensource/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  copyclip_opensource
+//
+//  Created by Max Dignan on 9/14/19.
+//  Copyright © 2019 Max Dignan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ copyclip_opensource/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  copyclip_opensource
+//
+//  Created by Max Dignan on 9/14/19.
+//  Copyright © 2019 Max Dignan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
